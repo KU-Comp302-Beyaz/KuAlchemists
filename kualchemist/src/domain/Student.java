19844304e3279525e28a1f32177f056f8ba07592
3cenaz <eegri21@ku.edu.tr>
2023-11-22 18:11:19 +0300

    CreatingStudentClass

---
+++ kualchemist/src/domain/Student.java
@@ -0,0 +1,18 @@
+package domain;
+
+import potion.Potion;
+
+public class Student {
+	
+	public Student() {
+		
+	}
+	
+
+	public AlchemyMarker testOnStudent(Potion p) {
+		//////
+		
+		AlchemyMarker a = null;
+		return a;
+	}
+}
