4178866b669c846a2e3f995934a5af71815afe4b
Alper Şahin <alpersahin21@ku.edu.tr>
2023-11-23 17:40:09 +0300

    Theories modified

--- kualchemist/src/theorydeduction/DeductionBoard.java
+++ kualchemist/src/theorydeduction/DeductionBoard.java
@@ -1,5 +1,20 @@
 package theorydeduction;
 
 public class DeductionBoard {
+	
+	private DeductionGrid deductionGrid;
 
+	public DeductionGrid getDeductionGrid() {
+		return deductionGrid;
+	}
+
+	public void setDeductionGrid(DeductionGrid deductionGrid) {
+		this.deductionGrid = deductionGrid;
+	}
+	
+	
+	
+	
+	
+	
 }
--- kualchemist/src/theorydeduction/Theory.java
+++ kualchemist/src/theorydeduction/Theory.java
@@ -1,5 +1,34 @@
 package theorydeduction;
 
+import domain.Alchemical;
+import domain.Player;
+
 public class Theory {
 
+	private Player owner;
+	private Alchemical alchemical;
+	
+	public Theory (Player p, Alchemical a) {
+		
+		this.owner = p;
+		this.alchemical = a;
+	}
+
+	public Player getOwner() {
+		return owner;
+	}
+
+	public void setOwner(Player owner) {
+		this.owner = owner;
+	}
+
+	public Alchemical getAlchemical() {
+		return alchemical;
+	}
+
+	public void setAlchemical(Alchemical alchemical) {
+		this.alchemical = alchemical;
+	}
+	
+	
 }
