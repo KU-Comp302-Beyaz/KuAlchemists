deacd3706650812324d0ae0f60db3ee9c2769a30
szulaldemirtas <100294970+szulaldemirtas@users.noreply.github.com>
2023-11-30 01:32:17 +0300

    Setting PotionBrewingAreaDisplay framework as gridbag layout

--- kualchemist/src/ui/PotionBrewingAreaDisplay.java
+++ kualchemist/src/ui/PotionBrewingAreaDisplay.java
@@ -12,12 +12,21 @@ import javax.swing.BoxLayout;
 import java.awt.Component;
 import java.awt.Dimension;
 import java.awt.SystemColor;
+import java.awt.GridBagLayout;
+import java.awt.GridBagConstraints;
+import java.awt.Insets;
+import javax.swing.JDesktopPane;
+import javax.swing.JToolBar;
+import java.awt.GridLayout;
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+import javax.swing.LayoutStyle.ComponentPlacement;
+import javax.swing.UIManager;
 
 public class PotionBrewingAreaDisplay extends JFrame {
 
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
-	private JPanel experimentPanel;
 	private JPanel potionSalePanel;
 	private JLabel lblNewLabel;
 
@@ -46,23 +55,31 @@ public class PotionBrewingAreaDisplay extends JFrame {
         contentPane = new JPanel();
         contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
         setContentPane(contentPane);
-        this.setExtendedState(JFrame.MAXIMIZED_BOTH); // automatically extends frame to desktop size (full size)
-        contentPane.setLayout(new BoxLayout(contentPane, BoxLayout.X_AXIS));
-
-        //Make Experiment Panel
-        experimentPanel = new JPanel();
-        experimentPanel.setAlignmentX(Component.LEFT_ALIGNMENT);
-        experimentPanel.setBackground(SystemColor.activeCaption);
-        experimentPanel.setPreferredSize(new Dimension ((int) (this.getWidth() * 0.7), this.getHeight()));
-        contentPane.add(experimentPanel);
-
-        //Potion Sale Panel
-        potionSalePanel = new JPanel();
-        potionSalePanel.setAlignmentX(Component.RIGHT_ALIGNMENT);
-        potionSalePanel.setPreferredSize(new Dimension ((int) (this.getWidth() * 0.3), this.getHeight()));
-        potionSalePanel.setBackground(SystemColor.textHighlight);
-        contentPane.add(potionSalePanel);
+        GridBagLayout gbl_contentPane = new GridBagLayout();
+        gbl_contentPane.columnWidths = new int[]{0, 0};
+        gbl_contentPane.rowHeights = new int[]{0, 0, 0};
+        gbl_contentPane.columnWeights = new double[]{0.7, 0.3};
+        gbl_contentPane.rowWeights = new double[]{1.0, Double.MIN_VALUE};
+        contentPane.setLayout(gbl_contentPane);
         
+        JPanel experimentPanel = new JPanel();
+        GridBagConstraints gbc_experimentPanel = new GridBagConstraints();
+        gbc_experimentPanel.insets = new Insets(0, 0, 5, 5);
+        gbc_experimentPanel.fill = GridBagConstraints.BOTH;
+        gbc_experimentPanel.gridx = 0;
+        gbc_experimentPanel.gridy = 0;
+        contentPane.add(experimentPanel, gbc_experimentPanel);
+        
+        JPanel potionSalePanel = new JPanel();
+        potionSalePanel.setBackground(UIManager.getColor("FormattedTextField.selectionBackground"));
+        GridBagConstraints gbc_potionSalePanel = new GridBagConstraints();
+        gbc_potionSalePanel.insets = new Insets(0, 0, 5, 0);
+        gbc_potionSalePanel.fill = GridBagConstraints.BOTH;
+        gbc_potionSalePanel.gridx = 1;
+        gbc_potionSalePanel.gridy = 0;
+        contentPane.add(potionSalePanel, gbc_potionSalePanel);
+        potionSalePanel.setLayout(new GridLayout(1, 0, 0, 0));
+          
 
 	}
 	
