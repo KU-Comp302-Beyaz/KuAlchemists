af6847c78f9f045e70c52bd742c19a7834a9538e
szulaldemirtas <100294970+szulaldemirtas@users.noreply.github.com>
2023-11-29 23:53:07 +0300

    PotionBrewingAreaDisplay update

--- kualchemist/src/ui/PotionBrewingAreaDisplay.java
+++ kualchemist/src/ui/PotionBrewingAreaDisplay.java
@@ -1,21 +1,28 @@
 package ui;
 
 import java.awt.EventQueue;
+import java.awt.FlowLayout;
 
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.Box;
+import javax.swing.BoxLayout;
+import java.awt.Component;
+import java.awt.SystemColor;
 
 public class PotionBrewingAreaDisplay extends JFrame {
 
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
+	private JPanel panel1;
+	private JPanel panel2;
+	private JLabel lblNewLabel;
 
 	/**
 	 * Launch the application.
 	 */
-	
-	/*
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
@@ -27,24 +34,38 @@ public class PotionBrewingAreaDisplay extends JFrame {
 				}
 			}
 		});
-	}*/
+	}
 
 	/**
 	 * Create the frame.
 	 */
 	public PotionBrewingAreaDisplay() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		//setBounds(100, 100, 450, 300);
-		this.setExtendedState(JFrame.MAXIMIZED_BOTH); // automatically extends frame to desktop size (full size)
-		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setBounds(100, 100, 450, 300);
+        contentPane = new JPanel();
+        contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+        setContentPane(contentPane);
+        contentPane.setLayout(new BoxLayout(contentPane, BoxLayout.X_AXIS));
 
-		setContentPane(contentPane);
+        panel1 = new JPanel();
+        panel1.setBackground(SystemColor.activeCaption);
+        contentPane.add(panel1);
+
+        contentPane.add(Box.createHorizontalStrut(10));
+
+        panel2 = new JPanel();
+        panel2.setBackground(SystemColor.textHighlight);
+        contentPane.add(panel2);
+        
+        lblNewLabel = new JLabel("Sell Potion");
+        panel2.add(lblNewLabel);
+		
+		
 	}
 	
+    
     public void initialize() {
 		
 		setVisible(true);
 	}
-
 }
