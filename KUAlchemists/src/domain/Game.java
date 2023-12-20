8763836675470dbc1ea0578cc76516f662f4499c
Starcik <ptopal21@ku.edu.tr>
2023-12-20 23:21:30 +0300

    Added InitializePlayers() and Changed player1 2 etc to players array

--- KUAlchemists/src/domain/Game.java
+++ KUAlchemists/src/domain/Game.java
@@ -14,17 +14,12 @@ import ui.IngredientStorageDisplay;
 import ui.LogInWindow;
 
 public class Game {
-	
-	//WE SHOULD USE THE STATE PATTERN IN HEAD FIRST DESIGN PATTERNS CHAPTER 10
-	//states like: LOGIN_WINDOW, PLAYER1S_TURN, PLAYER2S_TURN, PLAYER1_WON, PLAYER2_WON, GAME_PAUSED etc.
 
 	//fields
-	public static Controller controller = null;
-	public static Player player1;
-	public static Player player2;
+	private static Controller controller = null;
+	private static Player currPlayer;
 	
-	//WE COULD ADD A currentPlayer TO USE FOR THE FUNCTIONS IN THE selectController()
-	public static Player currPlayer;
+	private static Player[] players = new Player[4];
 	
 	//Controller as enum
 	public enum Controller {
@@ -45,32 +40,50 @@ public class Game {
 		return gameSingleton;
 	}
 	
-	//MAIN function
+	//Main function
 	public static void main(String[] args) {
 		
 		//Displaying the Login Window:
-		LogInWindow menuWindow = new LogInWindow();
-		menuWindow.displayMenuWindow();
+		LogInWindow loginWindow = LogInWindow.getInstance(); 
+		loginWindow.displayLogInWindow();
 		
-		//Creation of player1:
-		String username1 = menuWindow.getFirstUsername();
-		int chosenAvatarIndex1 = menuWindow.getFirstAvatarIndex();
-		player1 = new Player(username1, chosenAvatarIndex1);
+		int numberOfPlayers = loginWindow.getNumberOfPlayers();
 
-		//Creation of player2:
-		String username2 = menuWindow.getSecondUsername();
-		int chosenAvatarIndex2 = menuWindow.getSecondAvatarIndex();
-		player2 = new Player(username2, chosenAvatarIndex2);
+		numberOfPlayers = 2; //for now erase later
 		
-		//just for now, will be implemented later
-		currPlayer = player1;
+		initializePlayers(loginWindow,players,numberOfPlayers);
 		initializeBoard();
-		player1.getIngredientCards().add(IngredientStorage.getInstance().getIngredientCards().get(0));
-		player1.getIngredientCards().add(IngredientStorage.getInstance().getIngredientCards().get(1));
-	
 
 	}
 	
+	/**
+	 * Initializes players for OFFLINE mode using numberOfPlayers.
+	 * Gives players 2 ingredient cards from ingredients deck.
+	 * Gives players 10 gold.
+	 * @param loginWindow
+	 * @param players
+	 * @param numberOfPlayers
+	 */
+	public static void initializePlayers(LogInWindow loginWindow, Player[] players, int numberOfPlayers) {
+		String username;
+		int chosenAvatarIndex;
+		int j = 0;
+		for (int i = 0; i < numberOfPlayers; i++) {
+			username = loginWindow.getFirstUsername();
+			chosenAvatarIndex = loginWindow.getFirstAvatarIndex();
+			players[i] = new Player(username,chosenAvatarIndex);
+			
+			players[i].getIngredientCards().add(IngredientStorage.getInstance().getIngredientCards().get(j++));
+			players[i].getIngredientCards().add(IngredientStorage.getInstance().getIngredientCards().get(j++));
+			
+			players[i].setGoldBalance(10);
+		}
+		currPlayer = players[0];
+	}
+	
+	/**
+	 * Initializes board
+	 */
 	public static void initializeBoard() {
 		IngredientStorageDisplay.getInstance().constructAllImagesDeck(IngredientController.getInstance().giveAllCardsToIngredientStorageDisplay());
 	}
@@ -126,6 +139,17 @@ public class Game {
 	public void setCurrPlayer(Player currPlayer) {
 		Game.currPlayer = currPlayer;
 	}
+
+	public static Player[] getPlayers() {
+		return players;
+	}
+
+	public void setPlayers(Player[] players) {
+		Game.players = players;
+	}
+
+	
+	
 
 
 	
