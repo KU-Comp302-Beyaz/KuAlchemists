5bf4e22a2cf2d497fa17ba6607dba7b1b1ca9d24
3cenaz <eegri21@ku.edu.tr>
2023-12-27 10:41:02 +0300

    add expected value. check and pass the test.

--- KUAlchemists/src/domain/ingredients/Alchemical.java
+++ KUAlchemists/src/domain/ingredients/Alchemical.java
@@ -10,17 +10,27 @@ import domain.theorydeduction.AlchemyMarker;
 public class Alchemical {
 	
 	private List<AlchemyMarker> aspects = new ArrayList<>();
+	/*
+	AlchemyMarker red;
+	AlchemyMarker green; 
+	AlchemyMarker blue;
+	*/
 	private ImageIcon alchemicalPhoto;
 	
 	
 	public Alchemical (AlchemyMarker red, AlchemyMarker green, AlchemyMarker blue, ImageIcon alchemicalPhoto) {
-		
+	
 		this.aspects.add(red);
 		this.aspects.add(green);
 		this.aspects.add(blue);
+		/*
+		this.red = red;
+		this.green = green;
+		this.blue = blue;*/
 		this.alchemicalPhoto = alchemicalPhoto;
 	}
 
+	
 	public List<AlchemyMarker> getAspects() {
 		return aspects;
 	}
@@ -29,10 +39,33 @@ public class Alchemical {
 		this.aspects = aspects;
 	}
 
+	
+
+/*
+	public AlchemyMarker getRed() {
+		return red;
+	}
+	public void setRed(AlchemyMarker red) {
+		this.red = red;
+	}
+
+	public AlchemyMarker getGreen() {
+		return green;
+	}
+	public void setGreen(AlchemyMarker green) {
+		this.green = green;
+	}
+
+	public AlchemyMarker getBlue() {
+		return blue;
+	}
+	public void setBlue(AlchemyMarker blue) {
+		this.blue = blue;
+	}
+*/
 	public ImageIcon getAlchemicalPhoto() {
 		return alchemicalPhoto;
 	}
-
 	public void setAlchemicalPhoto(ImageIcon alchemicalPhoto) {
 		this.alchemicalPhoto = alchemicalPhoto;
 	}
--- KUAlchemists/src/test/MakeExperiment.java
+++ KUAlchemists/src/test/MakeExperiment.java
@@ -41,67 +41,67 @@ public class MakeExperiment {
 		/////////	TEST MAKE POTION	////////////
 		
 		result = pba.makePotion(ing[0],ing[1]).getAlchemyMarker();
-		System.out.println("ingredient 1-2: output:" + result.getColor() + " " + result.getSign() + " expected:  ");
+		System.out.println("ingredient 1-2: output:" + result.getColor() + " " + result.getSign() + " expected: null null ");
 		result = pba.makePotion(ing[0],ing[2]).getAlchemyMarker();
-		System.out.println("ingredient 1-3: output:" + result.getColor() + " " + result.getSign() + " expected: ");
+		System.out.println("ingredient 1-3: output:" + result.getColor() + " " + result.getSign() + " expected: green -");
 		result = pba.makePotion(ing[0],ing[3]).getAlchemyMarker();
-		System.out.println("ingredient 1-4: output:" + result.getColor() + " " + result.getSign() + " expected: ");
+		System.out.println("ingredient 1-4: output:" + result.getColor() + " " + result.getSign() + " expected: green -");
 		result = pba.makePotion(ing[0],ing[4]).getAlchemyMarker();
-		System.out.println("ingredient 1-5: output:" + result.getColor() + " " + result.getSign() + " expected: ");
+		System.out.println("ingredient 1-5: output:" + result.getColor() + " " + result.getSign() + " expected: blue -");
 		result = pba.makePotion(ing[0],ing[5]).getAlchemyMarker();
-		System.out.println("ingredient 1-6: output:" + result.getColor() + " " + result.getSign() + " expected: ");
+		System.out.println("ingredient 1-6: output:" + result.getColor() + " " + result.getSign() + " expected: red +");
 		result = pba.makePotion(ing[0],ing[6]).getAlchemyMarker();
-		System.out.println("ingredient 1-7: output:" + result.getColor() + " " + result.getSign() + " expected: ");
+		System.out.println("ingredient 1-7: output:" + result.getColor() + " " + result.getSign() + " expected: blue -");
 		result = pba.makePotion(ing[0],ing[7]).getAlchemyMarker();
-		System.out.println("ingredient 1-8: output:" + result.getColor() + " " + result.getSign() + " expected: ");
+		System.out.println("ingredient 1-8: output:" + result.getColor() + " " + result.getSign() + " expected: red +");
 		
 		result = pba.makePotion(ing[1],ing[2]).getAlchemyMarker();
-		System.out.println("ingredient 2-3: output:" + result.getColor() + " " + result.getSign() + " expected: ");
+		System.out.println("ingredient 2-3: output:" + result.getColor() + " " + result.getSign() + " expected: red -");
 		result = pba.makePotion(ing[1],ing[3]).getAlchemyMarker();
-		System.out.println("ingredient 2-4: output:" + result.getColor() + " " + result.getSign() + " expected: ");
+		System.out.println("ingredient 2-4: output:" + result.getColor() + " " + result.getSign() + " expected: blue +");
 		result = pba.makePotion(ing[1],ing[4]).getAlchemyMarker();
-		System.out.println("ingredient 2-5: output:" + result.getColor() + " " + result.getSign() + " expected: ");
+		System.out.println("ingredient 2-5: output:" + result.getColor() + " " + result.getSign() + " expected: green +");
 		result = pba.makePotion(ing[1],ing[5]).getAlchemyMarker();
-		System.out.println("ingredient 2-6: output:" + result.getColor() + " " + result.getSign() + " expected: ");
+		System.out.println("ingredient 2-6: output:" + result.getColor() + " " + result.getSign() + " expected: green +");
 		result = pba.makePotion(ing[1],ing[6]).getAlchemyMarker();
-		System.out.println("ingredient 2-7: output:" + result.getColor() + " " + result.getSign() + " expected: ");
+		System.out.println("ingredient 2-7: output:" + result.getColor() + " " + result.getSign() + " expected: red -");
 		result = pba.makePotion(ing[1],ing[7]).getAlchemyMarker();
-		System.out.println("ingredient 2-8: output:" + result.getColor() + " " + result.getSign() + " expected: ");
+		System.out.println("ingredient 2-8: output:" + result.getColor() + " " + result.getSign() + " expected: blue +");
 		
 		result = pba.makePotion(ing[2],ing[3]).getAlchemyMarker();
-		System.out.println("ingredient 3-4: output:" + result.getColor() + " " + result.getSign() + " expected: ");
+		System.out.println("ingredient 3-4: output:" + result.getColor() + " " + result.getSign() + " expected: blue +");
 		result = pba.makePotion(ing[2],ing[4]).getAlchemyMarker();
-		System.out.println("ingredient 3-5: output:" + result.getColor() + " " + result.getSign() + " expected: ");
+		System.out.println("ingredient 3-5: output:" + result.getColor() + " " + result.getSign() + " expected: red -");
 		result = pba.makePotion(ing[2],ing[5]).getAlchemyMarker();
-		System.out.println("ingredient 3-6: output:" + result.getColor() + " " + result.getSign() + " expected: ");
+		System.out.println("ingredient 3-6: output:" + result.getColor() + " " + result.getSign() + " expected: null null");
 		result = pba.makePotion(ing[2],ing[6]).getAlchemyMarker();
-		System.out.println("ingredient 3-7: output:" + result.getColor() + " " + result.getSign() + " expected: ");
+		System.out.println("ingredient 3-7: output:" + result.getColor() + " " + result.getSign() + " expected: green -");
 		result = pba.makePotion(ing[2],ing[7]).getAlchemyMarker();
-		System.out.println("ingredient 3-8: output:" + result.getColor() + " " + result.getSign() + " expected: ");
+		System.out.println("ingredient 3-8: output:" + result.getColor() + " " + result.getSign() + " expected: blue +");
 		
 		result = pba.makePotion(ing[3],ing[4]).getAlchemyMarker();
-		System.out.println("ingredient 4-5: output:" + result.getColor() + " " + result.getSign() + " expected: ");
+		System.out.println("ingredient 4-5: output:" + result.getColor() + " " + result.getSign() + " expected: null null");
 		result = pba.makePotion(ing[3],ing[5]).getAlchemyMarker();
-		System.out.println("ingredient 4-6: output:" + result.getColor() + " " + result.getSign() + " expected: ");
+		System.out.println("ingredient 4-6: output:" + result.getColor() + " " + result.getSign() + " expected: red +");
 		result = pba.makePotion(ing[3],ing[6]).getAlchemyMarker();
-		System.out.println("ingredient 4-7: output:" + result.getColor() + " " + result.getSign() + " expected: ");
+		System.out.println("ingredient 4-7: output:" + result.getColor() + " " + result.getSign() + " expected: green -");
 		result = pba.makePotion(ing[3],ing[7]).getAlchemyMarker();
-		System.out.println("ingredient 4-8: output:" + result.getColor() + " " + result.getSign() + " expected: ");
+		System.out.println("ingredient 4-8: output:" + result.getColor() + " " + result.getSign() + " expected: red +");
 		
 		result = pba.makePotion(ing[4],ing[5]).getAlchemyMarker();
-		System.out.println("ingredient 5-6: output:" + result.getColor() + " " + result.getSign() + " expected: ");
+		System.out.println("ingredient 5-6: output:" + result.getColor() + " " + result.getSign() + " expected: blue -");
 		result = pba.makePotion(ing[4],ing[6]).getAlchemyMarker();
-		System.out.println("ingredient 5-7: output:" + result.getColor() + " " + result.getSign() + " expected: ");
+		System.out.println("ingredient 5-7: output:" + result.getColor() + " " + result.getSign() + " expected: red -");
 		result = pba.makePotion(ing[4],ing[7]).getAlchemyMarker();
-		System.out.println("ingredient 5-8: output:" + result.getColor() + " " + result.getSign() + " expected: ");
+		System.out.println("ingredient 5-8: output:" + result.getColor() + " " + result.getSign() + " expected: green +");
 		
 		result = pba.makePotion(ing[5],ing[6]).getAlchemyMarker();
-		System.out.println("ingredient 6-7: output:" + result.getColor() + " " + result.getSign() + " expected: ");
+		System.out.println("ingredient 6-7: output:" + result.getColor() + " " + result.getSign() + " expected: blue -");
 		result = pba.makePotion(ing[5],ing[7]).getAlchemyMarker();
-		System.out.println("ingredient 6-8: output:" + result.getColor() + " " + result.getSign() + " expected: ");
+		System.out.println("ingredient 6-8: output:" + result.getColor() + " " + result.getSign() + " expected: green +");
 		
 		result = pba.makePotion(ing[6],ing[7]).getAlchemyMarker();
-		System.out.println("ingredient 7-8: output:" + result.getColor() + " " + result.getSign() + " expected: ");
+		System.out.println("ingredient 7-8: output:" + result.getColor() + " " + result.getSign() + " expected: null null");
 	}
 
 }
