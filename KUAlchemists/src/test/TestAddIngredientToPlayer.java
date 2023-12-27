53bb9051e7f89fbb96ff555dc010b60e7fe5fe01
Starcik <ptopal21@ku.edu.tr>
2023-12-27 21:18:48 +0300

    Added assertThrows tests but did not test them yet

--- KUAlchemists/src/test/TestAddIngredientToPlayer.java
+++ KUAlchemists/src/test/TestAddIngredientToPlayer.java
@@ -6,9 +6,18 @@ import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import domain.Player;
+import domain.ingredients.IngredientController;
+import domain.ingredients.IngredientStorage;
+
 class TestAddIngredientToPlayer {
+	
+	IngredientController ingredientController;
+	IngredientStorage ingredientStorage;
+	Player player;
 
 	@BeforeAll
 	static void setUpBeforeClass() throws Exception {
@@ -20,6 +29,9 @@ class TestAddIngredientToPlayer {
 
 	@BeforeEach
 	void setUp() throws Exception {
+		ingredientController = IngredientController.getInstance(); //should these be new IngredientController() ??
+		ingredientStorage = IngredientStorage.getInstance();
+		player = new Player(null,0);
 	}
 
 	@AfterEach
@@ -27,8 +39,35 @@ class TestAddIngredientToPlayer {
 	}
 
 	@Test
-	final void testAddIngredientToPlayer() {
+	void testAddIngredientToPlayer() {
 		fail("Not yet implemented"); // TODO
+	}
+	
+	@Test
+	@DisplayName("Null player in function argument should throw NullPointerException")
+	void testNullPlayer() {
+		assertThrows(NullPointerException.class, () -> ingredientController.addIngredientToPlayer(null));
+	}
+	
+	@Test
+	@DisplayName("Null player.ingredientCards should throw NullPointerException")
+	void testNullPlayerIngredientCards() {
+		player.setIngredientCards(null);
+		assertThrows(NullPointerException.class, () -> ingredientController.addIngredientToPlayer(player));
+	}
+	
+	@Test
+	@DisplayName("Null ingredientStorage.ingredientStorageSingleton should throw NullPointerException")
+	void testNullIngredientStorage() {
+		ingredientStorage = null;
+		assertThrows(NullPointerException.class, () -> ingredientController.addIngredientToPlayer(player));
+	}
+	
+	@Test
+	@DisplayName("Null ingredientStorage.ingredientCards should throw NullPointerException")
+	void testNullIngredientStorageIngredientCards() {
+		ingredientStorage.setIngredientCards(null);;
+		assertThrows(NullPointerException.class, () -> ingredientController.addIngredientToPlayer(player));
 	}
 
 }
