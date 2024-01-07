8ba05f74e3925e9ed3a6962972252de6ec50e540
barissaka <barissaka@localhost>
2024-01-07 23:25:51 +0300

    added squires modifies effects

--- KUAlchemists/src/domain/artifact/ArtifactController.java
+++ KUAlchemists/src/domain/artifact/ArtifactController.java
@@ -18,23 +18,30 @@ private static ArtifactController ArtifactControllerSingleton = new ArtifactCont
 	//player buys the artifact using this function
 	/// if the usage of the artifact is denoted as immidiate rather than adding the artifact to the player's artifact list uses the artifact immidiately
 	public void buyArtifact(Artifact artifact, Player player) {
-		
+		//REQUIRES: artifact and player is not null
+		//MODIFIES: the player
+		//EFFECTS: the player has an artifact added to their artifact storage
 		if(player.getGoldBalance() >= 3) {
-			if(artifact.usage == "immidiate") {
-				player.setTurnNumber(player.getTurnNumber() - 1);
-				player.setGoldBalance(player.getGoldBalance() - 3);
-				useArtifact(artifact, player);
-			}
-			
-			else{
-				player.setGoldBalance(player.getGoldBalance() - 3); 
-				player.addArtifact(artifact);
-				//update the lastUsed field of the artifact
+			if(player.getTurnNumber() >= 1) {
+				if(artifact.usage == "immidiate") {
+					player.setTurnNumber(player.getTurnNumber() - 1);
+					player.setGoldBalance(player.getGoldBalance() - 3);
+					useArtifact(artifact, player);
+				}
+				
+				else{
+					player.setGoldBalance(player.getGoldBalance() - 3); 
+					player.addArtifact(artifact);
+					//update the lastUsed field of the artifact
+				}
 			}
 		}
 	}
 	
 	public void useArtifact(Artifact artifact, Player player){
+		//REQUIRES: artifact and player is not null
+		//MODIFIES: ---
+		//EFFECTS: ---
 		if(artifact.usage == "perround") {
 			// cannot do anything for this yet as there is no way to know what round the player is currently in
 		}
